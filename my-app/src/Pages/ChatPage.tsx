abb3d1ba806afb660e9f0423fd811cb3d4b92a17
Zhancheng <zhanchengdev@gmail.com>
2021-08-29 15:31:21 -0400

    Reset post css version

--- my-app/src/Pages/ChatPage.tsx
+++ my-app/src/Pages/ChatPage.tsx
@@ -4,9 +4,13 @@ import { IChatSummary } from "../Models/ChatSummary.type";
 import '../styles/output.css';
 
 export const ChatPage: React.FC<{}> = () => {
-    return <div className="md:w-1/3 lg:w-1/4">
-        <ChatSummaryList summaries={getSampleChatSummary()}/>
-    </div>
+    return
+    <div>
+        <div className="md:w-1/3 lg:w-1/4">
+          <ChatSummaryList summaries={getSampleChatSummary()}/>
+        </div>
+    </div> 
+    
 }
 
 export const getSampleChatSummary = () => {
